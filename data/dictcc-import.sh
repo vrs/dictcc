31bef8bd2967823a7cbb77eadeaee42949b35d2b
vrs <vrs>
2015-01-10 15:54:01 +0100

    * move dictcc-import.sh to data/

--- data/dictcc-import.sh
+++ data/dictcc-import.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # step 1: get the dictionary file
 # http://www1.dict.cc/translation_file_request.php
-# step 2: run this script
-cd data/ || exit 1
+# step 2: edit and run this script
 rm -f dictcc.db deen-sanitized.txt
 grep "^[^#	]\+	[^	]\+	[^	]*$" dict.cc-de-en-1382462239.txt |sed 's/"/\\"/g'> deen-sanitized.txt
 sqlite3 dictcc.db <<EOF
