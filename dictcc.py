4feaf99737622df7b71dd5afcd3981321e2a4b3d
vrs <vrs@users.noreply.github.com>
2020-06-12 13:57:17 +0200

    python3

--- dictcc.py
+++ dictcc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sqlite3
 import sys
 import os
@@ -7,15 +7,15 @@ db = sqlite3.connect('%s/data/dictcc.db' % os.path.dirname(os.path.realpath(sys.
 
 while True:
     try:
-        query = raw_input('\33[30m\33[43mdictcc>\033[49m\33[39m ').strip()
+        query = input('\33[30m\33[43mdictcc>\033[49m\33[39m ').strip()
         c = db.cursor()
         # you weren't going to stick this behind a http server were you?
         q='select de,en from deen where deen match "%s" order by (de like "%s %%") + (en like "%s %%") asc' % (query, query, query)
-        print q
+        print(q)
         c.execute(q)
         for (de,en) in c.fetchall():
-            print u'{:<50}\t{}'.format(de, en)
+            print(u'{:<50}\t{}'.format(de, en))
     except EOFError:
         db.close()
-        print
+        print()
         sys.exit()
